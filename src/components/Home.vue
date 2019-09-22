b01de8352ea894d6c81601c1a73e1d89515b434b
Craig Berry <cberry@chewy.com>
2019-09-22 18:11:10 -0400

    add background image and cleanup

---
+++ src/components/Home.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-container fill-height>
+
+    <v-layout
+      text-center
+      wrap
+    >
+
+      <v-flex class="d-flex align-center justify-center">
+        <p class="subheading font-weight-thin white--text display-3">
+          More coming soon...
+        </p>
+      </v-flex>
+
+    </v-layout>
+  </v-container>
+</template>
